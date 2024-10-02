df3db592d4038791569b626052ce69fb5c5850f2
Vijak Khajornritdacha <vijak13019@hotmail.com>
2024-10-02 10:42:54 +0700

    feat: initialize guarantor

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { CreditorModule } from './creditor/creditor.module';
 import { ConfigModule } from '@nestjs/config';
 import { DebtorModule } from './debtor/debtor.module';
 import { LoanModule } from './loan/loan.module';
+import { GuarantorModule } from './guarantor/guarantor.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ScheduleModule.forRoot(), CreditorModule, DebtorModule, LoanModule],
+  imports: [ConfigModule.forRoot(), ScheduleModule.forRoot(), CreditorModule, DebtorModule, LoanModule, GuarantorModule],
   controllers: [AppController],
   providers: [AppService],
 })
