786660320c0e5bcd71c6e501f3802e6454112777
Vijak Khajornritdacha <vijak13019@hotmail.com>
2024-10-02 21:51:41 +0700

    fix: pass test, lint

--- src/debtor/debtor.controller.spec.ts
+++ src/debtor/debtor.controller.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DebtorController } from './debtor.controller';
 import { DebtorService } from './debtor.service';
+import { ConfigModule } from '@nestjs/config';
+import { FirebaseModule } from '../firebase/firebase.module';
 
 describe('DebtorController', () => {
   let controller: DebtorController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot(), FirebaseModule],
       controllers: [DebtorController],
       providers: [DebtorService],
     }).compile();
--- src/debtor/debtor.service.spec.ts
+++ src/debtor/debtor.service.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DebtorService } from './debtor.service';
+import { ConfigModule } from '@nestjs/config';
+import { FirebaseModule } from '../firebase/firebase.module';
 
 describe('DebtorService', () => {
   let service: DebtorService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot(), FirebaseModule],
       providers: [DebtorService],
     }).compile();
 
