afc4d71c7d3233b667a96c3d52eac97a66f26328
SecretKr <kraiwit.wit@gmail.com>
2024-11-01 09:54:03 +0700

    add TODO

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -36,6 +36,7 @@ import { RefreshAuthGuard } from './refresh-auth.guard';
             return res.status(400).send(token);
         }
         console.log(token)
+        // TODO: change redirect url (to app)
         return res.redirect(`http://localhost:3000/api/auth/test?token=${token.accessToken}&refreshToken=${token.refreshToken}`);
     }
 
