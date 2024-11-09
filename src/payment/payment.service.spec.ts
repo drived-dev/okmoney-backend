ad7d42e276981f4676679a5ba383060ca481947e
Vijak Khajornritdacha <vijak13019@hotmail.com>
2024-11-10 00:05:11 +0700

    fix: run test

--- src/payment/payment.service.spec.ts
+++ src/payment/payment.service.spec.ts
@@ -2,13 +2,20 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentService } from './payment.service';
 import { FirebaseModule } from '../firebase/firebase.module';
 import { ConfigModule } from '@nestjs/config';
+import { DebtorModule } from '@/debtor/debtor.module';
+import { LoanModule } from '@/loan/loan.module';
 
 describe('PaymentService', () => {
   let service: PaymentService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [ConfigModule.forRoot(), FirebaseModule],
+      imports: [
+        ConfigModule.forRoot(),
+        FirebaseModule,
+        DebtorModule,
+        LoanModule,
+      ],
       providers: [PaymentService],
     }).compile();
 
