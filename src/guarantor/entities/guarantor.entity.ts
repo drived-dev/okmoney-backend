db22cdda951196014b78304204530b4f70009070
Vijak Khajornritdacha <vijak13019@hotmail.com>
2024-11-12 18:50:43 +0700

    feat: add strict type

--- src/guarantor/entities/guarantor.entity.ts
+++ src/guarantor/entities/guarantor.entity.ts
@@ -3,25 +3,27 @@ import { z } from 'zod';
 
 const e164PhoneNumberRegex = /^\+[1-9]\d{1,14}$/;
 
-export const GuarantorSchema = z.object({
-  id: z.string(),
-  firstName: z.string().min(1, 'First name is required'),
-  lastName: z.string().min(1, 'Last name is required'),
-  phoneNumber: z
-    .string()
-    .min(1, 'Phone number is required')
-    .regex(
-      e164PhoneNumberRegex,
-      'Phone number must be in E.164 format (+66819009000)',
-    ),
-  createdAt: z.coerce
-    .date()
-    .transform((date) => new Date(date).getTime())
-    .optional(),
-  updatedAt: z.coerce
-    .date()
-    .transform((date) => new Date(date).getTime())
-    .optional(),
-});
+export const GuarantorSchema = z
+  .object({
+    id: z.string(),
+    firstName: z.string().min(1, 'First name is required'),
+    lastName: z.string().min(1, 'Last name is required'),
+    phoneNumber: z
+      .string()
+      .min(1, 'Phone number is required')
+      .regex(
+        e164PhoneNumberRegex,
+        'Phone number must be in E.164 format (+66819009000)',
+      ),
+    createdAt: z.coerce
+      .date()
+      .transform((date) => new Date(date).getTime())
+      .optional(),
+    updatedAt: z.coerce
+      .date()
+      .transform((date) => new Date(date).getTime())
+      .optional(),
+  })
+  .strict();
 
 export class Guarantor extends createZodDto(GuarantorSchema) {}
--- src/payment/entities/payment.entity.ts
+++ src/payment/entities/payment.entity.ts
@@ -7,22 +7,24 @@ export enum PaymentType {
   TRANSFER = 'TRANSFER',
 }
 
-export const PaymentSchema = z.object({
-  id: z.string(),
-  loanId: z.string(),
-  creditorId: z.string(),
-  debtorId: z.string(),
-  amount: z.number().min(0, 'Amount is required'),
-  paymentDate: z.coerce
-    .date()
-    .transform((date) => new Date(date).getTime())
-    .optional(),
-  paymentType: z.nativeEnum(PaymentType),
-  imageUrl: z.string().optional(),
-  createdAt: z.coerce
-    .date()
-    .transform((date) => new Date(date).getTime())
-    .optional(),
-});
+export const PaymentSchema = z
+  .object({
+    id: z.string(),
+    loanId: z.string(),
+    creditorId: z.string(),
+    debtorId: z.string(),
+    amount: z.number().min(0, 'Amount is required'),
+    paymentDate: z.coerce
+      .date()
+      .transform((date) => new Date(date).getTime())
+      .optional(),
+    paymentType: z.nativeEnum(PaymentType),
+    imageUrl: z.string().optional(),
+    createdAt: z.coerce
+      .date()
+      .transform((date) => new Date(date).getTime())
+      .optional(),
+  })
+  .strict();
 
 export class Payment extends createZodDto(PaymentSchema) {}
