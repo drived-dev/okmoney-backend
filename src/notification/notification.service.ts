1abb43a5b293d9ef19092f8ed372c680a243df23
SecretKr <kraiwit.wit@gmail.com>
2025-03-24 22:15:18 +0700

    feat: change to THSMS

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -6,9 +6,9 @@ import { AxiosError } from 'axios';
 
 @Injectable()
 export class NotificationService {
-  private apiUrl = 'https://api-v2.thaibulksms.com/sms';
-  private apiKey = process.env.THAI_SMS_API;
-  private apiSecret = process.env.THAI_SMS_SECRET;
+  private apiUrl = 'https://thsms.com/api/send-sms';
+  private apiKey = process.env.TH_SMS_API;
+  private useSms = process.env.USE_SMS == 'YES';
 
   getHello(): string {
     return 'Hello World!';
@@ -18,39 +18,38 @@ export class NotificationService {
 
   async sendSms(to: string, message: string) {
     const data = {
-      msisdn: to, // Recipient phone number in international format
+      msisdn: [to], // Recipient phone number in international format
       message: message, // Your message text
-      sender: 'Demo', // Custom sender name (depends on your ThaiBulkSMS account)
-      force: 'standard', // Could be "standard" or "premium"
+      sender: 'Direct SMS',
     };
 
-    console.log('notification.service: mock function call ThaiSMS Api');
-    console.log(data);
-    // const headers = {
-    //   Authorization: `Basic ${Buffer.from(`${this.apiKey}:${this.apiSecret}`).toString('base64')}`,
-    //   'Content-Type': 'application/x-www-form-urlencoded',
-    // };
+    if (this.useSms) {
+      const headers = {
+        Authorization: `Basic ${Buffer.from(`${this.apiKey}`).toString('base64')}`,
+        'Content-Type': 'application/json',
+      };
 
-    // try {
-    //   const encodedData = qs.stringify(data);
-    //   const response = await lastValueFrom(
-    //     this.httpService.post(this.apiUrl, encodedData, { headers }),
-    //   );
-    //   return response.data; // ThaiBulkSMS API response
-    // } catch (error: unknown) {
-    //   // Type guard to check if 'error' is an instance of 'Error'
-    //   if (error instanceof AxiosError) {
-    //     // Safely access Axios-specific properties
-    //     console.log('Error Response:', error.response?.data);
-    //     console.log('Error Status:', error.response?.status);
-    //     console.log('Error Headers:', error.response?.headers);
-    //   } else if (error instanceof Error) {
-    //     // Handle generic errors
-    //     console.log(`Error: ${error.message}`);
-    //   } else {
-    //     console.log('An unknown error occurred');
-    //   }
-    // }
+      try {
+        const encodedData = qs.stringify(data);
+        const response = await lastValueFrom(
+          this.httpService.post(this.apiUrl, encodedData, { headers }),
+        );
+        return response.data;
+      } catch (error: unknown) {
+        if (error instanceof AxiosError) {
+          console.log('Error Response:', error.response?.data);
+          console.log('Error Status:', error.response?.status);
+          console.log('Error Headers:', error.response?.headers);
+        } else if (error instanceof Error) {
+          console.log(`Error: ${error.message}`);
+        } else {
+          console.log('An unknown error occurred');
+        }
+      }
+    } else {
+      console.log('notification.service: mock function call ThSMS Api');
+      console.log(data);
+    }
     return data;
   }
 }
